2438b3a039d7a5fcb16b7cfbb4af73bc7aa45fc8
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-28 14:55:04 +1100

    HOLLOWAY, Isabella: 2018-02-28 14:55:04

---
+++ user/Calculator 2.52.24 pm.py
@@ -0,0 +1,11 @@
+A = int(input("Enter a number: "))
+Actions = input("Add, Subtract, Divide or Multiply? ")
+B = int(input("Enter another number: "))
+if Actions == "Add":
+    print(A + B)
+if Actions == "Subtract":
+    print(A - B)
+if Actions == "Divide":
+    print(A / B)
+if Actions == "Multiply":
+    print(A * B)
---
+++ user/PigLatin.py
@@ -0,0 +1,3 @@
+a = str(input("Enter one word to translate: "))
+Vowel = ("a, e, i, o, u, A, E, I, O, U")
+
