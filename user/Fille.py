65353e97ba6c8eeb1c5e9fe087804cab1ddc672f
Isabella Holloway <hollowayiam@gmail.com>
2018-04-24 10:14:50 +1000

    functions

---
+++ user/Fille.py
@@ -0,0 +1,3 @@
+print('hello')
+name = input('What is your name? ')
+print('hello', name)
---
+++ user/Functions.py
@@ -0,0 +1,5 @@
+name = imput('What is your name?')
+def hello():
+    print('Hello! ')
+print('Hello! ',name )
+hello()
