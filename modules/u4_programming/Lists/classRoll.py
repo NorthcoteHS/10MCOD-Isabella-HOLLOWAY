17b16106b0eb1e82cd55025a8845829339bb68e8
Isabella Holloway <hollowayiam@gmail.com>
2018-05-02 14:06:55 +1000

    classRoll completed with 2 challenges

--- modules/u4_programming/Lists/classRoll.py
+++ modules/u4_programming/Lists/classRoll.py
@@ -1,3 +1,22 @@
+"""
+Prog: Student Roll
+Name: Isabella Holloway
+Date: 02/05/18
+Desc: Creates list, add student, remove student, change student, with 2 challenges
+"""
+
 roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah']
 student2 = roll[2]
 print(student2)
+enrolment = len(roll)
+print(enrolment)
+roll.append('James')
+print(roll[10])
+del roll[2]
+print(roll[2])
+roll[5] = 'Mike'
+print(roll[5])
+list.sort(roll)
+print(roll)
+list.reverse(roll)
+print(roll)
